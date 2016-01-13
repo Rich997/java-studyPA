38d039b14fe60f25dbd7b4f5934efd18a6d1eb0f
Andrey <richm997@gmail.com>
2016-01-13 17:13:38 +0200

    adding minus and multiply

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,24 @@
 public class Calculate {
-	public static void main(String[] args){
+	public static void main(String[] arg){
 		System.out.println("Calculate...");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int sum = first + second;
+		System.out.println("Sum " + sum);
+		
+		int subtraction=first-second;
+		System.out.println("Subtraction " + subtraction);
+		
+		int multi = first*second;
+		System.out.println("Multi " + multi);
+		
+		double division;
+		division=first/second;
+		System.out.println("Division " + division);
+		
+		
+		
+		
 	}
 	
 }
